84e613d75ecde522dfc03c31cf11920099f329e8
cheonkio <heelun8525@naver.com>
2025-07-21 23:58:18 +0900

    cors 허용

--- src/main/java/org/scoula/config/ServletConfig.java
+++ src/main/java/org/scoula/config/ServletConfig.java
@@ -28,6 +28,7 @@ public class ServletConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://cheonkio.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 
